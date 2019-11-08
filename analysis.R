94c92706fec7649166b742a21aa44ac97089fc36
Suzan Dilara Tokac <sdtokac@ua-visitor-ten-21-143-32.uniaccess.unimelb.edu.au>
2019-11-08 16:14:21 +1100

    add read data and make plot

--- analysis.R
+++ analysis.R
@@ -1,2 +1,26 @@
 #Analysis script for my awesome project 
 
+#create data
+apples <-data.frame(apples_id = as.factor(1:1000),
+                    weight = rnorm(1000, 5 , 1))
+
+write.csv(apples, 'data/apples_data.csv', row.names = FALSE)
+
+#read data
+apples <- read.csv('data/apples_data.csv')
+
+#process data
+
+apples$log_weight <- log(apples$weight)
+
+#graphing
+png('figs/hist_apple_weight.png')
+#here also you can define weight and height by adding ti next to the code by coma
+
+hist(apples$weight)
+dev.off()
+#the good thing about this function is that when you run the whole script 
+#you dont have to manually export each plot instead it will creat all of them for you in a folder
+
+
+
